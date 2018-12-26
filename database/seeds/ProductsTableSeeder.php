ee65919c48f3aac154383006863228181005763e
Francisco <Francisco@fmorales>
2018-12-25 23:09:15 -0300

    Seeders Categorias, Productos, Imagenes.

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use App\Product;
+use App\Category;
+use App\ProductImage;
 
 class ProductsTableSeeder extends Seeder
 {
@@ -12,7 +14,8 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        // Model factories
+        factory(Category::class, 5)->create();
         factory(Product::class, 100)->create();
+        factory(ProductImage::class, 200)->create();
     }
 }
