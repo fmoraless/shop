d83a6fcc10e48026964775438f6700ebcd3346ae
fmorales <fmorales@T460S-FMORALES.grupokaufmann.com>
2018-12-28 17:43:18 -0300

    CRUD de productos.

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/prueba', function(){
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin/products', 'ProductController@index'); //listado
+Route::get('/admin/products/create', 'ProductController@create'); //formulario
+Route::post('/admin/products/', 'ProductController@store'); //registrar
