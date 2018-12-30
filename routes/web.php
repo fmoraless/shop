589f39a213ab1435132cba718f9871348c572d75
Francisco <Francisco@fmorales>
2018-12-30 02:13:32 -0300

    Formulario de edición.

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin/products', 'ProductController@index'); //listado
 Route::get('/admin/products/create', 'ProductController@create'); //formulario
 Route::post('/admin/products/', 'ProductController@store'); //registrar
+
+Route::get('/admin/products/{id}/edit', 'ProductController@edit'); //formulario edición
+Route::post('/admin/products/{id}/edit', 'ProductController@update'); //actualiar
