1ffda0fb065e1ed9d60f0c4d49845b488790699f
fmoraless <franticzero@gmail.com>
2019-01-31 16:32:11 -0300

    Botones en la vista index.

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -40,15 +40,23 @@
             <td>{{ $product->category ? $product->category->name : 'General' }}</td>
             <td class="text-right">{{ $product->price }}</td>
             <td class="td-actions text-right">
-                <button type="button" rel="tooltip" title="Ver producto" class="btn btn-info btn-simple btn-xs">
-                    <i class="fa fa-info"></i>
-                </button>
-                <a href="{{ url('/admin/products/'. $product->id.'/edit') }}" rel="tooltip" title="Editar producto" class="btn btn-success btn-simple btn-xs">
-                    <i class="fa fa-edit"></i>
-                </a>
-                <button type="button" rel="tooltip" title="Eliminar" class="btn btn-danger btn-simple btn-xs">
-                    <i class="fa fa-times"></i>
-                </button>
+                <form method="post" action="{{ url('/admin/products/'.$product->id) }}">
+                    {{ csrf_field() }}
+                    {{ method_field('DELETE') }}
+
+                    <a href="{{ url('/products/'.$product->id) }}" rel="tooltip" title="Ver producto" class="btn btn-info btn-simple btn-xs" target="_blank">
+                        <i class="fa fa-info"></i>
+                    </a>
+                    <a href="{{ url('/admin/products/'.$product->id.'/edit') }}" rel="tooltip" title="Editar producto" class="btn btn-success btn-simple btn-xs">
+                        <i class="fa fa-edit"></i>
+                    </a>
+                    <a href="{{ url('/admin/products/'.$product->id.'/images') }}" rel="tooltip" title="Imágenes del producto" class="btn btn-warning btn-simple btn-xs">
+                        <i class="fa fa-image"></i>
+                    </a>
+                    <button type="submit" rel="tooltip" title="Eliminar" class="btn btn-danger btn-simple btn-xs">
+                        <i class="fa fa-times"></i>
+                    </button>
+                </form>
             </td>
         </tr>
         @endforeach
