586e6b44f71474e0950373755781dfdf28ea0603
fmoraless <franticzero@gmail.com>
2019-01-31 16:25:28 -0300

    Validaciones en producto.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -20,6 +20,24 @@ class ProductController extends Controller {
     public function store(Request $request) {
         // registrar el nuevo producto en la bd
         //dd($request->all());
+
+        // validar
+        $messages = [
+            'name.required' => 'Es necesario ingresar un nombre para el producto.',
+            'name.min' => 'El nombre del producto debe tener al menos 3 caracteres.',
+            'description.required' => 'La descripción corta es un campo obligatorio',
+            'description.max' => 'La descripción corta solo admite hasta 200 caracteres.',
+            'price.required' => 'Es obligatorio definir un precio para el producto',
+            'price.numeric' => 'Ingrese un precio válido',
+            'price.min' => 'No se admiten valores negativos',
+        ];
+        $rules = [
+          'name' => 'required|min:3',
+          'description' => 'required|max:200',
+          'price' => 'required|numeric|min:0',
+        ];
+        $this->validate($request, $rules,$messages);
+
         $product = new Product();
         $product->name = $request->input('name');
         $product->description = $request->input('description');
@@ -39,6 +57,24 @@ class ProductController extends Controller {
     public function update(Request $request, $id) {
         // registrar el nuevo producto en la bd
         //dd($request->all());
+
+        // validar
+        $messages = [
+            'name.required' => 'Es necesario ingresar un nombre para el producto.',
+            'name.min' => 'El nombre del producto debe tener al menos 3 caracteres.',
+            'description.required' => 'La descripción corta es un campo obligatorio',
+            'description.max' => 'La descripción corta solo admite hasta 200 caracteres.',
+            'price.required' => 'Es obligatorio definir un precio para el producto',
+            'price.numeric' => 'Ingrese un precio válido',
+            'price.min' => 'No se admiten valores negativos',
+        ];
+        $rules = [
+            'name' => 'required|min:3',
+            'description' => 'required|max:200',
+            'price' => 'required|numeric|min:0',
+        ];
+        $this->validate($request, $rules,$messages);
+
         $product = Product::find($id);
         $product->name = $request->input('name');
         $product->description = $request->input('description');
@@ -48,5 +84,15 @@ class ProductController extends Controller {
         
         //redirigir al usuario luego de insertar
         return redirect('/admin/products');
+    }
+
+    public function destroy($id)
+    {
+
+        $product = Product::find($id);
+        $product->delete(); //DELETE
+
+        //redirigir al usuario luego de insertar
+        return back();
     }
 }
