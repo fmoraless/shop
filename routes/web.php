1f30b2fbc47c3d95eee3b77620773d384b2a3a82
fmoraless <franticzero@gmail.com>
2019-01-31 16:26:13 -0300

    Agrupar rutas para middleware Admin.

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,11 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/admin/products', 'ProductController@index'); //listado
-Route::get('/admin/products/create', 'ProductController@create'); //formulario
-Route::post('/admin/products/', 'ProductController@store'); //registrar
-
-Route::get('/admin/products/{id}/edit', 'ProductController@edit'); //formulario edición
-Route::post('/admin/products/{id}/edit', 'ProductController@update'); //actualiar
+Route::middleware(['auth', 'admin'])->group(function() {
+    Route::get('/admin/products', 'ProductController@index'); //listado
+    Route::get('/admin/products/create', 'ProductController@create'); //formulario
+    Route::post('/admin/products/', 'ProductController@store'); //registrar
+    Route::get('/admin/products/{id}/edit', 'ProductController@edit'); //formulario edición
+    Route::post('/admin/products/{id}/edit', 'ProductController@update'); //actualiar
+    Route::delete('/admin/products/{id}', 'ProductController@destroy'); //formulario Eliminar
+});
