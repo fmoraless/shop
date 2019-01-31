592d08b0d8b0911fc708e76a162386c909ae0857
fmoraless <franticzero@gmail.com>
2019-01-31 16:31:11 -0300

    Validacion de errores en Creacion y edicion de productos.

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -13,6 +13,17 @@
 
         <div class="section">
             <h2 class="title text-center">Registrar nuevo producto</h2>
+
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
             <form method="post" action="{{ url('/admin/products') }}">
                 {{ csrf_field() }}
 
@@ -20,26 +31,29 @@
                     <div class="col-sm-6">
                         <div class="form-group label-floating">
                             <label class="control-label">Nombre producto</label>
-                            <input type="text" class="form-control" name="name">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group label-floating">
                             <label class="control-label">Precio producto</label>
-                            <input type="number" class="form-control" name="price">
+                            <input type="number" class="form-control" name="price" value="{{ old('price') }}">
                         </div>
                     </div>
                 </div>
                 
                     <div class="form-group label-floating">
                         <label class="control-label">Descripción corta</label>
-                        <input type="text" class="form-control" name="description">
+                        <input type="text" class="form-control" name="description" value="{{ old('description') }}">
                     </div>
                 
 
-                <textarea class="form-control" placeholder="Descripción extensa de producto" rows="5" name="long_description"></textarea>
+                <textarea class="form-control" placeholder="Descripción extensa de producto" rows="5" name="long_description">
+                    {{ old('long_description') }}
+                </textarea>
 
                 <button class="btn btn-primary">Registrar Producto</button>
+                <a href="{{ url('admin/products') }}" class="btn btn-default">Cancelar</a>
             </form>
 
         </div>
