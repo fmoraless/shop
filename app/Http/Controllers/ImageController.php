1dcb61e31b0b4ba4a2e7fff473b8d472617ef61e
fmoraless <franticzero@gmail.com>
2019-02-06 00:15:08 -0300

    Guardar y eliminar imagenes de un producto.

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Product;
+use App\ProductImage;
 use Illuminate\Http\Request;
+use File;
 
 class ImageController extends Controller
 {
@@ -14,12 +16,39 @@ class ImageController extends Controller
         $images = $product->images;
         return view('admin.products.images.index')->with(compact('product', 'images'));
     }
-    public function store()
+    public function store(Request $request, $id)
     {
+        //guardar img en nuestro proyecto
+        $file = $request->file('photo');
+        $path = public_path() . '/images/products';
+        $fileName = uniqid() . $file->getClientOriginalName();
+        $moved = $file->move($path, $fileName);
 
+
+        // crear 1 registro en la tabla product_images
+        if ($moved) {
+            $productImage = new ProductImage();
+            $productImage->image = $fileName;
+            //$productImage->featured = false;
+            $productImage->product_id = $id;
+            $productImage->save(); // insert
+        }
+        return back();
     }
-    public function destroy()
+    public function destroy(Request $request, $id)
     {
-
-    }
+        //Eliminar el archivo
+        $productImage = ProductImage::find($request->image_id);
+        if (substr($productImage->image, 0,4) !== "http") {
+            $deleted = true;
+        }else{
+            $fullPath = public_path() . '/images/products' . $productImage->image;
+            $deleted = File::delete($fullPath);
+        }
+        //Eliminar el archivo de la img en la BD
+        if ($deleted){
+            $productImage->delete();
+        }
+        return back();
+        }
 }
